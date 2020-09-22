58fc4dc548c24f3a2b931e22797e447f5db94365
Elena Spivak <spivak.e.v@gmail.com>
2020-09-22 11:33:44 +0300

    feat: add slide and slide description switch in the team section

--- js/index.js
+++ js/index.js
@@ -26,7 +26,7 @@ const findCurrentSlideIndex = function (slideList) {
   }
 }
 
-
+// По нажатию на стрелку ">" в слайдере прячем текущий слайд и показываем следующий. Для слайдеров, где только картинки. Например, в секции pets.
 const showNextSlide = function (section) {
   let nextButton = section.querySelector('.slider__btn_next');
   let slideList = section.querySelectorAll('.slider__slide');
@@ -55,12 +55,7 @@ const showNextSlide = function (section) {
   })
 }
 
-/* А если все спрятаны?
-Или больше 1 показаны?
-Добавить обработку этих случаев (?)
-Выводить хотя бы в консоль предупреждение!
- */
-
+// По нажатию на стрелку "<" в слайдере прячем текущий слайд и показываем предыдущий. Для слайдеров, где только картинки. Например, в секции pets.
 const showPrevSlide = function (section) {
   let prevButton = section.querySelector('.slider__btn_prev');
   let slideList = section.querySelectorAll('.slider__slide');
@@ -89,9 +84,80 @@ const showPrevSlide = function (section) {
   })
 }
 
+// По нажатию на стрелку ">" в слайдере прячем текущий слайд и описание под ним и показываем следующий слайд и его описание. (Для секций team и reviews).
+const showNextSlideAndDescription = function (section) {
+  let nextButton = section.querySelector('.slider__btn_next');
+  let slideList = section.querySelectorAll('.slider__slide');
+  let slideDescriptionList = section.querySelectorAll('.slide-description');
+
+  nextButton.addEventListener('click', function () {
+    // находим индекс текущего элемента
+    let currentIndex = findCurrentSlideIndex(slideList);
+
+    // находим индекс следующего элемента
+    let nextIndex;
+
+    if (currentIndex === slideList.length - 1) {
+      nextIndex = 0;
+    } else {
+      nextIndex = currentIndex + 1;
+    }
+
+    let currentSlide = slideList[currentIndex];
+    let nextSlide = slideList[nextIndex];
+
+    // прячем текущий слайд и описание
+    let currentSlideDescription = slideDescriptionList[currentIndex];
+    currentSlide.classList.add('hide');
+    currentSlideDescription.classList.add('hide');
+
+    // показываем следующий слайд и описание
+    let nextSlideDescription = slideDescriptionList[nextIndex];
+    nextSlide.classList.remove('hide');
+    nextSlideDescription.classList.remove('hide');
+  })
+}
+
+// По нажатию на стрелку "<" в слайдере прячем текущий слайд и описание под ним и показываем предыдущий слайд и его описание. (Для секций team и reviews).
+const showPrevSlideAndDescription = function (section) {
+  let prevButton = section.querySelector('.slider__btn_prev');
+  let slideList = section.querySelectorAll('.slider__slide');
+  let slideDescriptionList = section.querySelectorAll('.slide-description');
+
+  prevButton.addEventListener('click', function () {
+    // находим индекс текущего элемента
+    let currentIndex = findCurrentSlideIndex(slideList);
+
+    // находим индекс следующего элемента
+    let prevIndex;
+
+    if (currentIndex === 0) {
+      prevIndex = slideList.length - 1;
+    } else {
+      prevIndex = currentIndex - 1;
+    }
+
+    let currentSlide = slideList[currentIndex];
+    let prevSlide = slideList[prevIndex];
+
+    // прячем текущий слайд и описание
+    let currentSlideDescription = slideDescriptionList[currentIndex];
+    currentSlide.classList.add('hide');
+    currentSlideDescription.classList.add('hide');
+
+    // показываем следующий слайд и описание
+    let prevSlideDescription = slideDescriptionList[prevIndex];
+    prevSlide.classList.remove('hide');
+    prevSlideDescription.classList.remove('hide');
+  })
+}
 
 let petSection = document.querySelector('.pets');
+let teamSection = document.querySelector('.team');
 
 questionToggle();
 showNextSlide(petSection);
 showPrevSlide(petSection);
+
+showNextSlideAndDescription(teamSection);
+showPrevSlideAndDescription(teamSection);
